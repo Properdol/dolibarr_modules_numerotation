850aea85a3391b72f80b0568dfcccc4f48e0d075
iouston <4319513+iouston@users.noreply.github.com>
2025-05-19 22:50:32 +0200

    add importblock cvm

--- index.php
+++ index.php
@@ -169,6 +169,7 @@ $dolibarr_modules_numerotation = array(
 	array('number' => '4462737',		'name' => 'automentiontva',							'description'  => 		'Ajoute une mention selon les taux de tva présents sur le document'),
 	array('number' => '4462738',		'name' => 'quickrelance',							'description'  => 		'Ajoute des colonnes dans la liste des devis pour des actions rapides de relance'),
 	array('number' => '4462739',		'name' => 'debugiou',								'description'  => 		'Affiche le debug php pour un utilisateur ou un autre'),
+	array('number' => '4462740',		'name' => 'importblock',							'description'  => 		'Permet d\importer des blocks de mesure pour CVM France'),
 	
 	array('number' => '',				'name' => 'stv_formulaire',							'description'  => 		'fork du module formulaire citique pour suivi production stv'),
 	array('number' => '',				'name' => 'stv_signalement',						'description'  => 		'fork du module signalement citique pour suivi production stv'),
